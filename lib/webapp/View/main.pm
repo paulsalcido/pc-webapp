abe152744bb9312444027363ef86b5ccb9c6e335
Paul Salcido <psalcido@manta.com>
2012-04-26 15:50:13 -0400

    Bootstrap files.

---
+++ lib/webapp/View/main.pm
@@ -0,0 +1,36 @@
+package webapp::View::main;
+
+use strict;
+use warnings;
+
+use base 'Catalyst::View::TT';
+
+__PACKAGE__->config(
+    TEMPLATE_EXTENSION => '.tt',
+    render_die => 1,
+);
+
+=head1 NAME
+
+webapp::View::main - TT View for webapp
+
+=head1 DESCRIPTION
+
+TT View for webapp.
+
+=head1 SEE ALSO
+
+L<webapp>
+
+=head1 AUTHOR
+
+Paul Salcido,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
---
+++ t/view_main.t
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+use Test::More;
+use Test::More;
+
+BEGIN { use_ok 'webapp::View::main' }
+
+done_testing();
